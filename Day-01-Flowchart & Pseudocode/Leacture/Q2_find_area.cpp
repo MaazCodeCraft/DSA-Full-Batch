23aca019355caf6e1314b003ee07bfaaaeae8728
Maaz Ur Rahman <maaz4cs@gmail.com>
2024-12-05 20:15:54 -0800

    Add files via upload

---
+++ Day-01-Flowchart & Pseudocode/Leacture/Q2_find_area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int a;
+
+  cout << "Please Enter Total Length:  ";
+  cin >> a;
+
+  int area = a*a;
+
+  cout << "Area of the square is:  " << area << endl;
+
+  return 0;
+}
