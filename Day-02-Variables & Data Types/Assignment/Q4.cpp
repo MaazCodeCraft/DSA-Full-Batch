c64b57286d7ed470ac309f7da877aafb077eb9cd
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-01-08 23:03:43 -0800

    Add files via upload

---
+++ Day-02-Variables & Data Types/Assignment/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define PI 3.14
+using namespace std;
+
+int main () {
+    
+    int r;
+
+    cout << "Enter radius : ";
+    cin >> r;
+
+    cout << "Area : " << PI*r*r << endl;
+
+    return 0;
+}
