0ff41bc220b836aa1eb361523ceefb7b7162b8a8
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-01-09 07:47:31 -0800

    Add files via upload

---
+++ Day-03-Operators/Code/01-Constant.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    
+    const int num = 25;
+    int num2 = 25;
+    num2 = 35;
+    const float PI = 3.14;
+
+    cout << num << " " << PI << endl;
+
+    return 0;
+}
