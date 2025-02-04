60d57164ec6835757af3259c3ce90cfeeaf36e5b
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-02-04 22:50:01 +0500

    Add files via upload

---
+++ Day-05-Loops/Assignment/01-Task.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int  main () {
+    int num;
+    cout << "Enter Number : ";
+    cin >> num;
+    
+    for (int i = 0; i < num; i++){
+        cout << "****" << endl;
+    }
+    return 0;
+}
